59cd65065920b61620584673bba4c2d98d38c27c
Martin Flischman <martincodes@icloud.com>
2024-09-03 15:43:45 +0200

    Initial Commit

---
+++ Chronicle/ChronicleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronicleApp.swift
+//  Chronicle
+//
+//  Created by Martin on 2024/09/03.
+//
+
+import SwiftUI
+
+@main
+struct ChronicleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
